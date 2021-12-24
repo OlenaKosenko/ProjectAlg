44398534e09f2b16984f3a38361c75df15c551db
Olena Kosenko <olena.kosenko.mail@gmail.com>
2021-12-23 16:47:01 -0800

    Add Algorithm

--- src/main/java/LuckyTicket.java
+++ src/main/java/LuckyTicket.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LuckyTicket {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyTicket {
+
+    public boolean isTicketLucky(int ticketNumber) {
+        List<Integer> numbers = new ArrayList<>();
+
+        do {
+           numbers.add(ticketNumber % 10);
+           ticketNumber /= 10;
+        } while (ticketNumber > 0);
+
+        int listSize = numbers.size();
+
+        if (listSize % 2 != 0) {
+            return false;
+        }
+
+        int firstHalfSum = 0;
+        int secondHalfSum = 0;
+
+        for (int i = 0; i < listSize; i++) {
+           if (i < listSize / 2) {
+               firstHalfSum += numbers.get(i);
+           } else {
+               secondHalfSum += numbers.get(i);
+           }
+        }
+
+        return firstHalfSum == secondHalfSum;
+    }
 }
--- src/test/java/LuckyTicketTest.java
+++ src/test/java/LuckyTicketTest.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class LuckyTicketTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LuckyTicketTest {
+
+
+    @Test
+    public void testLuckyTicket() {
+        int ticketNumber = 832661;
+        boolean expectedResult = true;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket2() {
+        int ticketNumber = 213985;
+        boolean expectedResult = false;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket3() {
+        int ticketNumber = 6701;
+        boolean expectedResult = false;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket4() {
+        int ticketNumber = 0;
+        boolean expectedResult = false;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket5() {
+        int ticketNumber = -22;
+        boolean expectedResult = false;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket6() {
+        int ticketNumber = 232;
+        boolean expectedResult = false;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testLuckyTicket7() {
+        int ticketNumber = 2350;
+        boolean expectedResult = true;
+
+        LuckyTicket luckyTicket = new LuckyTicket();
+        boolean actualResult = luckyTicket.isTicketLucky(ticketNumber);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
